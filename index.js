f38b9993e834b3d52af08ca9d41f7250a3571ecd
mjkim0928 <june573166@naver.com>
2024-05-07 01:12:35 +0900

    return all

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { createSchema, createYoga } from 'graphql-yoga'
 import { readFileSync } from 'node:fs';
 const typeDefs = readFileSync('./graphql/schema.graphql', 'utf8');
 import resolvers from "./graphql/resolvers";
-const endPoint = 'INSERT_ROUTE'; //엔드포인트 경로: http://IP주소:포트번호/
+
 const yoga = createYoga({
     schema: createSchema({
         typeDefs,
@@ -14,7 +14,5 @@ const yoga = createYoga({
 const server = createServer(yoga)
 
 server.listen(4000, () => {
-
-    
-    console.info('Server is running on '+ endPoint)
+    console.info('Server is running on http://localhost:4000/graphql')
 })
