9e45f995e23a052a7ea629ce448e8076d8a110d2
mjkim0928 <june573166@naver.com>
2024-05-07 00:08:19 +0900

    revert commit

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-import { ServerResponse, createServer } from 'node:http'
+import { createServer } from 'node:http'
 import { createSchema, createYoga } from 'graphql-yoga'
 import { readFileSync } from 'node:fs';
 const typeDefs = readFileSync('./graphql/schema.graphql', 'utf8');
 import resolvers from "./graphql/resolvers";
 const endPoint = 'http://192.168.219.101:3000/'; //엔드포인트 경로: http://IP주소:포트번호/
 const yoga = createYoga({
-    //landingPage: True,
-    graphqlEndpoint: endPoint,
     schema: createSchema({
         typeDefs,
         resolvers
