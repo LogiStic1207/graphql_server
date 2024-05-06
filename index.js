169c7d0291c247deaf2a1a4c45585bc34f49f5e9
mjkim0928 <june573166@naver.com>
2024-05-06 23:32:53 +0900

    babel-node reinstall

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { createSchema, createYoga } from 'graphql-yoga'
 import { readFileSync } from 'node:fs';
 const typeDefs = readFileSync('./graphql/schema.graphql', 'utf8');
 import resolvers from "./graphql/resolvers";
-const endPoint = 'INSERT_ROUTE'; //엔드포인트 경로: http://IP주소:포트번호/
+const endPoint = 'http://192.168.219.101:3000/'; //엔드포인트 경로: http://IP주소:포트번호/
 const yoga = createYoga({
     //landingPage: True,
     graphqlEndpoint: endPoint,
@@ -15,7 +15,7 @@ const yoga = createYoga({
 
 const server = createServer(yoga)
 
-server.listen(4000, () => {
+server.listen(3000, () => {
 
     
     console.info('Server is running on '+ endPoint)
